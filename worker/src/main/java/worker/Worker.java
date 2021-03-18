9289b4426f3ff0f5e2b8328b90a54031932c8d6f
Corentin Mas <corentinmas@yahoo.com>
2021-03-18 15:37:09 +0100

    [Fix] Functional program, just need to use env variables

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -8,8 +8,8 @@ import org.json.JSONObject;
 class Worker {
   public static void main(String[] args) {
     try {
-        Jedis redis = connectToRedis("127.0.0.1");
-        Connection dbConn = connectToDB("127.0.0.1");
+        Jedis redis = connectToRedis("redis"); // todo use env
+        Connection dbConn = connectToDB("db"); // todo use env
 
         System.err.println("Watching vote queue");
 
